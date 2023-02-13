c756f0371db66314cf0eef22df1a337e0bb9747f
jasmine <jasminesimxy@gmail.com>
2023-02-13 23:27:30 +1100

    check what needs to be added in the team.html and ask BCS for assistant with the generating card part for html

--- generateHTML.js
+++ generateHTML.js
@@ -20,6 +20,8 @@ function generateHTML (allEmployee) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Job Plug Profile</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+            <link rel="stylesheet" href="./dist/style.css">
+
 
         </head>
         <body>

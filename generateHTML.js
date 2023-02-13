3c4fffae3cbca0638a604ebc8bebb5971f192bff
jasmine <jasminesimxy@gmail.com>
2023-02-13 23:11:51 +1100

    added html eleemnts in generateHTML.js

--- generateHTML.js
+++ generateHTML.js
@@ -14,26 +14,29 @@ function generateHTML (allEmployee) {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-            <style type="text/css">
-            #textin {
-                width: 500px;
-                height: 150px;
-            }
-            #textout {
-                font-family: courier;
-                font-size: 12px;
-                width: 40em;
-                height: 200px;
-                resize: none;
+            <title>Job Plug Profile</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
-            </style>
         </head>
         <body>
-            ${generateManagerCard(allEmployee)}
-            ${generateEngineerCard(allEmployee)}
-            ${generateInternCard(allEmployee)}
+        <div class="container text-center">
+        <div class="row">
+          <div class="col">Job Plug Profile</div>
+
+          <main class="container">
+          <section class="row">
+          <section class="row justify-content-around">
+          <div class="col-12 col-sm-6 col-lg-4 mb-3">
+          <div class =card  style="width: 18rem;">
+          ${generateEngineerCard(allEmployee)}
+          ${generateInternCard(allEmployee)}
+          ${generateManagerCard(allEmployee)}
+          </div>
+          </div>
+          </section>
+          </section>
+          </main>
+
         </body>
     </html>
     `

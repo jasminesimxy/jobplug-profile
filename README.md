faa28d37fdf4ad2066488c4be9e846bcd1e75bba
jasmine <jasminesimxy@gmail.com>
2023-02-13 22:39:07 +1100

    worked on the readme fil, need to finish it and finish off the css styling/bootstrap in generateHTML.js

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# jobplug-profile
+# Job Plug Profile
+
+  ## Description
+  This project is about run a command line application where we can prompt for the team member and their information. With those information entered into the command line, we can generate a styled HTML browser.  
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+
+ ## Installation
+
+The required step to install my project is through clicking on the links attached below:
+
+Deployed application link : 
+Github Repo URL link : https://github.com/jasminesimxy/jobplug-profile 
+
+My repository is called Job Plug Profile and it contains all the codes and file needed to create the website (test: 
+
+## Usage
+
+
+Deployed application link : 
--- generateHTML.js
+++ generateHTML.js
@@ -15,8 +15,20 @@ function generateHTML (allEmployee) {
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Document</title>
-            <link> // bootstrap
-            <link src='./css/stylesheet.css'>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+            <style type="text/css">
+            #textin {
+                width: 500px;
+                height: 150px;
+            }
+            #textout {
+                font-family: courier;
+                font-size: 12px;
+                width: 40em;
+                height: 200px;
+                resize: none;
+
+            </style>
         </head>
         <body>
             ${generateManagerCard(allEmployee)}
@@ -25,16 +37,13 @@ function generateHTML (allEmployee) {
         </body>
     </html>
     `
-    //loop over the employee and generate a html
-
-
-    // return that entire long template literal, i.e. all that html markup with embedded information about the employees
-    // return generatedHtml; // some string
  };
-
 
 
  module.exports = { generateHTML };
 
+     //loop over the employee and generate a html
+    // return that entire long template literal, i.e. all that html markup with embedded information about the employees
+    // return generatedHtml; // some string
 
 
